3a4d4e55c899007ac6d8e53a174932218dc4566e
waymakerweb <127877507+waymakerweb@users.noreply.github.com>
2023-09-19 19:04:21 -0500

    agregamos el index.html

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//boton
+$('.dropdown-trigger').dropdown();
+
+//collapsible
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+  //modal
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
